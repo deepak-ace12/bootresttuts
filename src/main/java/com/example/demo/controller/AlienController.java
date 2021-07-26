2f1bb6bffd74164486d45117c3d223efbe569658
Deepak Sharma <deepak15.sharma@paytminsurance.co.in>
2021-07-26 13:12:41 +0530

    delete and update

--- src/main/java/com/example/demo/controller/AlienController.java
+++ src/main/java/com/example/demo/controller/AlienController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -64,4 +66,18 @@ public class AlienController {
 		repo.save(alien);
 		return alien;
 	}
+	
+	@DeleteMapping("/alien/{aid}")
+	public String deleteAlien(@PathVariable int aid) {
+		Alien alien = repo.getById(aid);
+		repo.delete(alien);
+		return "Deleted";
+	}
+	
+	@PutMapping(path="/alien", consumes = {"application/json"})
+	public Alien saveOrUpdateAlien(@RequestBody Alien alien) {
+		repo.save(alien);
+		return alien;
+	}
+
 }
