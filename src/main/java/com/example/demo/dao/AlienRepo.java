9f1c363ae58b9682563a99c74e84760152bbc9d7
Deepak Sharma <deepak15.sharma@paytminsurance.co.in>
2021-07-26 12:30:51 +0530

    crud with json as response

--- src/main/java/com/example/demo/dao/AlienRepo.java
+++ src/main/java/com/example/demo/dao/AlienRepo.java
@@ -1,21 +1,18 @@
 package com.example.demo.dao;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.Alien;
 
-import java.util.List;
-
-public interface AlienRepo extends CrudRepository<Alien, Integer>{
-	List<Alien> findByTech(String tech);
-	
-	List<Alien> findByAidGreatherThan(int aid);
-	
-	//Writing complex queries
-	
-	@Query("from Alien where tech=?1 order by aname") //JPQL query
-	List<Alien> findByTechSorted(String tech);
+public interface AlienRepo extends JpaRepository<Alien, Integer>{
+//	List<Alien> findByTech(String tech);
+//	
+//	List<Alien> findByAidGreatherThan(int aid);
+//	
+//	//Writing complex queries
+//	
+//	@Query("from Alien where tech=?1 order by aname") //JPQL query
+//	List<Alien> findByTechSorted(String tech);
 	
 	
 }
